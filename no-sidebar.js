173eaf3802eb9a6041934f01e795ae98a49f7396
Christian G. Warden <cwarden@xerus.org>
2017-08-11 13:51:09 -0700

    Disable Loading Salesforce Promos On Login Screen
    Disable loading Salesforce promo iframes on login screen to avoid
    problem where loading of the iframe interupts choosing the saved
    credentials to use in Chrome.

--- no-sidebar.js
+++ no-sidebar.js
@@ -1,7 +1,7 @@
 console.log('no-sidebar loaded');
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
 	console.log(details.url);
-	if (details.url.match(/sidebar/)) {
+	if (details.url.match(/login-messages\/promos.html/)) {
 		return { cancel: true };
 	}
-}, { urls: ['https://*.amazon.com/*'] }, ['blocking']);
+}, { urls: ['https://*.salesforce.com/*'] }, ['blocking']);
